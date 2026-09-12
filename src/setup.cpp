d9ce0561717cd082bf3237fb37c7774c431abbfb
Ryan Yeung <ryeung0305@gmail.com>
2026-09-12 00:09:13 -0700

    added documentation in setup.cpp

--- src/setup.cpp
+++ src/setup.cpp
@@ -2,8 +2,14 @@
 #include "vex.h"
 #include "setup.h"
 
+//configure motors
 vex::motor rightMotorFront(vex::PORT14, vex::gearSetting::ratio18_1, false);
+
 vex::motor leftMotorFront(vex::PORT11, vex::gearSetting::ratio18_1, false);
+
 vex::motor rightMotorBack(vex::PORT13, vex::gearSetting::ratio18_1, false);
+
 vex::motor leftMotorBack(vex::PORT12, vex::gearSetting::ratio18_1, false);
+
+//configure controller
 vex::controller Controller1(vex::controllerType::primary);
