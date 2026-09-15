5e641002a8f2bf3c7b6d5846d8e449219d32846d
Ryan Yeung <ryeung0305@gmail.com>
2026-09-15 15:25:44 -0700

    added turn sensor

--- src/auto.cpp
+++ src/auto.cpp
@@ -1,25 +1,19 @@
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
 
 #include "vex.h"
 #include "setup.h"
 
+const int RIGHT_ANGLE_TURN = 60;
 
 void autonomous() {
 
     //TODO: USE INERTIAL SENSOR TO GET DATA
     InertialSensor.setHeading(0, rotationUnits::deg); // Reset the head of Inertial Sensor to 0 degrees
 
-
-    driveTrain.turnFor(right, 60, deg); // Turn to 90 degrees
+    while (InertialSensor.heading() < 90){
+        driveTrain.turnFor(right, RIGHT_ANGLE_TURN, deg); // Turn to 90 degrees
+    }
+    
 
   
 
