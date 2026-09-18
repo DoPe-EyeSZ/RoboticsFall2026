7097620a83e5332d6b32747fc41a7a59a6c2270d
DoPe-EyeSZ <ryeung0305@gmail.com>
2026-09-18 13:14:03 -0700

    got pid to work on both sides

--- src/auto.cpp
+++ src/auto.cpp
@@ -7,8 +7,8 @@
 brain Brain;
 
 void turnRight(){
-    const double target = 90;
-    const double Kp = 1;
+    const double target = InertialSensor.rotation(deg) + 90;
+    const double Kp = 0.5;
     const double Kd = 0.5;
 
     double err = 0;
@@ -43,11 +43,39 @@ void turnRight(){
 }
 
 void turnLeft(){
-    double currDeg = InertialSensor.rotation(deg);
-    while (currDeg - InertialSensor.rotation(deg) < 60){
-        driveTrain.turn(left);
+    const double target = InertialSensor.rotation(deg) - 90;
+    const double Kp = 0.5;
+    const double Kd = 0.5;
+
+    double err = 0;
+    double prevErr = 0;
+
+    double currDeg;
+    double derivative;
+    double motorPow;
+
+    while (true){
+        currDeg = InertialSensor.rotation(deg);
+
+        err = target - currDeg;
+
+        derivative = err - prevErr;
+
+        motorPow = (Kp*err) + (Kd*derivative);
+
+        leftWheels.spin(forward, motorPow, percent);
+        rightWheels.spin(reverse, motorPow, percent);
+
+        prevErr = err;
+
+        if (fabs(err) < 1){
+            break;
+        }
+        wait(10, msec);
+        
     }
-    driveTrain.stop();
+    leftWheels.stop();
+    rightWheels.stop();
 }
 
 void autonomous() {
@@ -56,7 +84,8 @@ void autonomous() {
     driveTrain.setTurnVelocity(20, percent);
 
 
-    turnRight();
+    turnLeft();
+
 
 
 
