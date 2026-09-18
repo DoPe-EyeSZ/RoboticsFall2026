a71e3e5ab6f08e76ea43aa08988ff59e57366964
brodyjurado <brodyjurado@gmail.com>
2026-09-18 09:41:21 -0700

    Added roadmap for odometry

--- src/auto.cpp
+++ src/auto.cpp
@@ -6,6 +6,14 @@
 const int RIGHT_ANGLE_TURN = 60;
 
 void autonomous() {
+  
+    //Gets current angle of the rotation sensor's shaft
+  double angle = Rotation1.angle(degrees);
+  double position( rotationUnits units );
+
+  //This prints the current angle/heading of the shaft
+  Brain.Screen.print(angle);
+  Brain.Screen.print(position);
 
     //TODO: USE INERTIAL SENSOR TO GET DATA
     InertialSensor.setHeading(0, rotationUnits::deg); // Reset the head of Inertial Sensor to 0 degrees
