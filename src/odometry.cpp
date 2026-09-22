0ee3590d33ade10b65f4e281e32fb0a0b28877fd
Brody Jurado <brodyjurado@gmail.com>
2026-09-22 11:59:22 -0700

    While loop condition is now time-based, PID and angle reading math has been updated based on Henry's advice

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -12,12 +12,16 @@ void autonomousOdometry() {
 
   RotationSensor.setPosition(0, rotationUnits::deg);
   //Gets current angle of the rotation sensor's shaft
-  double angle = RotationSensor.angle(degrees);
-  double position = RotationSensor.position(rotationUnits::deg);
+  double initialAngle = RotationSensor.position(rotationUnits::deg);
+  double finalAngle = initialAngle + (wheelRotations * 360); //360 degrees for a full rotation
 
   //This prints the current angle/heading of the shaft
-  Controller1.Screen.print(angle);
-  Controller1.Screen.print(position);
+  Controller1.Screen.print(initialAngle);
+  Controller1.Screen.print(finalAngle);
+  Controller1.Screen.newLine();
+  Controller1.Screen.print(RotationSensor.position(rotationUnits::deg));
+  Controller1.Screen.newLine();
+  Controller1.Screen.print(wheelRotations);
 
   //Rotation sensor PID, linear movement
   //Coefficients still need fine tuning
@@ -34,16 +38,29 @@ void autonomousOdometry() {
   RotationSensor.setPosition(0, rotationUnits::deg);
 
 
-  //Still need to correct this math and figure out how to send this data to the drivetrain
-  while (true) {
-    err = 5000 - RotationSensor.position(rotationUnits::deg); //Target angle is 90 degrees
-    integral += err;
-    derivative = err - last_err;
+  //New math, new while loop
+
+  int loopDelay = 20; //in milliseconds
+  int timeElapsed = 0; //in milliseconds
+  int timeRun = 5000; //in milliseconds
+
+  while (timeElapsed < timeRun) {
+    double currentAngle = RotationSensor.position(rotationUnits::deg);
+    err = finalAngle - currentAngle;
+
+    derivative = (err - last_err);
     double output = (kP * err) + (kD * derivative);
 
     leftMotorFront.spin(forward, output, voltageUnits::volt);
     rightMotorFront.spin(forward, output, voltageUnits::volt);
 
     last_err = err;
+
+    //Fixed time
+    wait(loopDelay, msec);
+    timeElapsed += loopDelay;
   }
+
+  leftMotorFront.stop(braketype::hold);
+  rightMotorFront.stop(braketype::hold);
 }
