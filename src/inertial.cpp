0db80e960288aab32da2fe16fe70c33d07e9886e
Brody Jurado <brodyjurado@gmail.com>
2026-09-22 12:50:28 -0700

    Suggestion/investigate drivetrain power further

--- src/inertial.cpp
+++ src/inertial.cpp
@@ -5,6 +5,8 @@
 
 void turnRight(){
     const double target = InertialSensor.rotation(deg) + 90;
+    //We should consider whether or not to consolidate the logic for the PID coefficients, not sure what kind of
+    //lateral asymmetry exists with the drivetrain
     const double Kp = 0.32;
     const double Kd = 0;
 
